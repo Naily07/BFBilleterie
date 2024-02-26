eeb5727583b14caee87eab0e969beb50dc6c1b3f
Haritiana <haritiananomenjanahary3@gmail.com>
2024-02-26 16:23:29 +0300

    Custome Modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        arapawa: {
+          800: "#2A4E62",
+          700: "#557181",
+        },
+      },
+
+      FontFamily: {
+        h1: ["Workbench"],
+      },
+    },
   },
   plugins: [],
-}
+};
