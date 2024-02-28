fc52d295cec9012cd535d4d90cbd9fb59a788f79
Haritiana <haritiananomenjanahary3@gmail.com>
2024-02-28 11:05:18 +0300

    Section mode de payement

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import PointDeVente from './pages/PoinDeVente/PvPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import PageNavigation from './pages/accueil/Accueil'
-import CreationEvenement from './pages/creationEvenement/CreationEvenement'
-import SingleEvent from './pages/singleEvent/SingleEvent'
-import Header from "./component/Header/HeaderMain"
-import Accueil from './pages/accueil/Accueil'
-import PageEvenement from './pages/pageEvenement/PageEvenement'
-import "./index.css"
+import PointDeVente from "./pages/PoinDeVente/PvPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageNavigation from "./pages/accueil/Accueil";
+import CreationEvenement from "./pages/creationEvenement/CreationEvenement";
+import SingleEvent from "./pages/singleEvent/SingleEvent";
+import Header from "./component/Header/HeaderMain";
+import Accueil from "./pages/accueil/Accueil";
+import PageEvenement from "./pages/pageEvenement/PageEvenement";
+import SingleEventUser from "./pages/singleEventUser/SingleEventUser";
+import "./index.css";
 
 function App() {
-  return(
-  <BrowserRouter>  
-    <Header/>
-    <Routes>     
-      <Route path='/' element={ <PageNavigation/> }/>
-      <Route path='/accueil' element={<Accueil/>}/>
-      <Route path='/creation-event' element={ <CreationEvenement/> }/>
-      <Route path='/single-event' element={ <SingleEvent/> }/>
-      <Route path='/point-de-vente' element={ <PointDeVente/> }/>
-      <Route path='/point-de-vente' element={ <PointDeVente/> }/>
-      <Route path='/page-evenement' element={ <PageEvenement/> }/>
-    </Routes>
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PageNavigation />} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/creation-event" element={<CreationEvenement />} />
+        <Route path="/single-event" element={<SingleEvent />} />
+        <Route path="/user" element={<SingleEventUser />} />
+        <Route path="/point-de-vente" element={<PointDeVente />} />
+        <Route path="/point-de-vente" element={<PointDeVente />} />
+        <Route path="/page-evenement" element={<PageEvenement />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
